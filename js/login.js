708e9e00d6869bf10a7642f289e1eaa38b33c373
YICA <you@example.comtu20040916@gmail.com>
2024-04-28 01:35:06 +0700

    duan3

--- js/login.js
+++ js/login.js
@@ -1,16 +1,50 @@
-function signup(e) {
-  event.preventDefault();
-  var username = document.getElementById("username").value;
-  var pw = document.getElementById("pw").value;
-  var mail = document.getElementById("mail").value;
-  var sdt = document.getElementById("sdt").value;
-  var user = {
-    username: username,
-    pw: pw,
-    mail: mail,
-    sdt: sdt,
+let apiUser = "http://localhost:3000/user";
+function Login() {
+  getUser(hanldelogin);
+}
+function getUser(callback) {
+  fetch(apiUser).then(function (res) {
+    return res.json().then(callback);
+  });
+}
+function hanldelogin(Data) {
+  let username = document.getElementById("username").value;
+  let pw = document.getElementById("pw").value;
+  Data.forEach((Data) => {
+    if (Data.username == username && Data.pw == pw) {
+      alert("đăng nhập thành công");
+      window.location.href = "./trang1.html";
+    }
+  });
+}
+
+function signup() {
+  handlecrateform();
+}
+function createUser(data) {
+  fetch(apiUser, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(function (res) {
+    return res.json();
+  });
+  if (data) {
+    alert("dang ki thanh cong");
+  }
+}
+function handlecrateform() {
+  let username = document.getElementById("username");
+  let pw = document.getElementById("pw");
+  let sdt = document.getElementById("sdt");
+  let mail = document.getElementById("mail");
+  let user = {
+    username: username.value,
+    pw: pw.value,
+    sdt: sdt.value,
+    mail: mail.value,
   };
-  var json = JSON.stringify(user);
-  localStorage.setItem(username, json);
-  alert("Dang ki thanh cong");
+  createUser(user);
 }
